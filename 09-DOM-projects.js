62839e0e0d0216b2134a00edfdc015f90f543194
irum-shah78 <irams4812@gmail.com>
2024-02-29 17:16:04 +0500

    Exercises

--- 09-DOM-projects.js
+++ 09-DOM-projects.js
@@ -1,3 +1,23 @@
+// Amazon Shipping Calculator
+function handleCost(event){
+  if(event.key === 'Enetr'){
+    calculateTotal();
+  }
+}
+
+
+function calculateTotal(){
+const inputElement = document.querySelector('.js-input');
+let cost = Number(inputElement.value);
+
+if(cost < 40){
+  cost = cost + 10;
+}
+document.querySelector('.js-cost').innerHTML = `$${cost}`;
+}
+
+
+
 // YT Subscribe Button Project
 function subscribeBtn() {
   const btnElement = document.querySelector('.js-btn');
@@ -6,4 +26,16 @@ function subscribeBtn() {
   } else {
     btnElement.innerHTML = 'Subscribe';
   }
+}
+
+// Excersie 9a-9b
+console.log(document.querySelector('.exercise-btn'));
+
+function exerciseBtn(){
+const exBtn =  document.querySelector('.exercise-btn2');
+if(exBtn.innerText === '9b'){
+exBtn.innerHTML = '9b Done!';
+} else {
+  exBtn.innerHTML = '9b';
+}
 }
