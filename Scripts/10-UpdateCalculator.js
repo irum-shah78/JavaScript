1522a0a8bea9b90006da0f2ad93e6bcfca7d19da
irum-shah78 <irams4812@gmail.com>
2024-03-03 12:05:09 +0500

    Calculator-Updation

---
+++ Scripts/10-UpdateCalculator.js
@@ -0,0 +1,18 @@
+// Exercise 10i-10j
+//let calculation = '';
+let calculation = localStorage.getItem('calculation') || '';
+showCalculation();
+
+function updateCalculation(value) {
+  calculation += value;
+  showCalculation()
+  localStorage.setItem('calculation', calculation);
+}
+
+function saveCalculation() {
+  localStorage.setItem('calculation', calculation);
+}
+
+function showCalculation() {
+  document.querySelector('.results').innerHTML = calculation;
+}
