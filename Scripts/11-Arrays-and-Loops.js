32df6aefd99fc7b9d747b37ff2ca4536981240d1
irum-shah78 <irams4812@gmail.com>
2024-03-09 18:57:26 +0500

    Lesson 11- Part 1

--- Scripts/11-Arrays-and-Loops.js
+++ Scripts/11-Arrays-and-Loops.js
@@ -1,3 +1,4 @@
+/*
 // Arrays
 const myArr = [10, 20, 30];
 console.log(myArr);
@@ -67,23 +68,24 @@ const num = Numbers[i];
 numDouble.push(num * 2);
 }
 console.log(numDouble);
+*/
 
 
 // Exercises - 11a
-const Numbers1 = [10,20,30];
+const Numbers1 = [10, 20, 30];
 Numbers1[2] = 99;
 console.log(Numbers1);
 
 // 11b
-function getLastValue(array){
-const lastIndex = array.length - 1;
-return array[lastIndex];
+function getLastValue(array) {
+  const lastIndex = array.length - 1;
+  return array[lastIndex];
 }
-console.log(getLastValue([10,15,40,5]));
+console.log(getLastValue([10, 15, 40, 5]));
 console.log(getLastValue(['Hello', 'World']));
 
 // 11c
-function arraySwap(array){
+function arraySwap(array) {
   const lastIndex = array.length - 1;
   const lastValue = array[lastIndex];
   const firstValue = array[0];
@@ -96,9 +98,121 @@ function arraySwap(array){
   // return array;
 }
 console.log(arraySwap(['Hello', 'World']));
-console.log(arraySwap([10,20,30,5]));
+console.log(arraySwap([10, 20, 30, 5]));
 
 // 11d
-for (let i = 0; i <= 10; i+2){
+for (let i = 0; i <= 10; i += 2) {
   console.log(i);
 }
+
+// 11e
+for (let i = 5; i >= 0; i--) {
+  console.log(i);
+}
+
+// 11f: 11d - 11e using while loop
+let i = 0;
+while (i <= 10) {
+  console.log(i);
+  i += 2;
+}
+
+let ind = 5;
+while (ind >= 0) {
+  console.log(ind);
+  ind--;
+}
+
+// 11g
+const array = [1, 2, 3, 4];
+let addOne = [];
+for (let i = 0; i < array.length; i++) {
+  addOne.push(array[i] + 1);
+}
+console.log(addOne);
+
+// 11h
+function addOnee(array) {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    result.push(array[i] + 1);
+  }
+  return result;
+}
+console.log(addOnee([1, 2, 3]));
+console.log(addOnee([-2, -1, 0, 99]));
+
+// 11i
+function addNum(array, num) {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    result.push(array[i] + num);
+  }
+  return result;
+}
+console.log(addNum([1, 2, 3], 2));
+console.log(addNum([1, 2, 3], 3));
+console.log(addNum([-2, -1, 0, 99], 2));
+
+// 11j
+function addArrays(array1, array2) {
+  let result = [];
+  for (let i = 0; i < array1.length; i++) {
+    result.push(array1[i] + array2[i]);
+  }
+  return result;
+}
+console.log(addArrays([1, 1, 2], [1, 1, 3]));
+
+// 11h
+function countPositive(nums) {
+  let result = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) {
+      result++;
+    }
+  }
+  return result;
+}
+console.log(countPositive([1, -3, 5]));
+console.log(countPositive([1, 2, -4, -4, 5]));
+
+// 11l - 11m
+function minMax(nums) {
+  const result = {
+    min: null,
+    max: null
+  }
+  for (let i = 0; i < nums.length; i++) {
+    const value = nums[i];
+
+    if (result.min === null || value < result.min) {
+      result.min = value;
+    }
+    if (result.max === null || value > result.max) {
+      result.max = value;
+    }
+  }
+  return result;
+}
+console.log(minMax([1, -3, 5]));
+console.log(minMax([-2, 3, -5, 7, 10]));
+console.log(minMax([]));
+console.log(minMax([3, 3]));
+
+// 11n
+function countWords(words) {
+  const result = {};
+
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    if (!result[word]) {
+      result[word] = 1;
+    } else {
+      result[word]++;
+    }
+  }
+  return result;
+}
+console.log(countWords(['apple', 'banana', 'orange', 'banana', 'apple', 'mango']));
+
